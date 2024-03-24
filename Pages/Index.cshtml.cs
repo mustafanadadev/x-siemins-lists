f9395ae1642ee5f9e48268f5ac153e912657e184
MoustafaNada <moustafa.nada@bi-technologies.net>
2024-03-25 01:19:07 +0200

    Refactor code to work on Ajax to not refreshing page. just processing and change values in time by ajax

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -7,12 +7,13 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Newtonsoft.Json;
 
-namespace x_siemins_dropdown.Pages
+namespace x_siemens_dropdown.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly IWebHostEnvironment _env;
         public List<BusinessCapability> businessCapabilities = new List<BusinessCapability>();
+
         public IndexModel(IWebHostEnvironment env)
         {
             _env = env;
@@ -22,6 +23,7 @@ namespace x_siemins_dropdown.Pages
             var jsonData = System.IO.File.ReadAllText(jsonFilePath);
             this.businessCapabilities = JsonConvert.DeserializeObject<List<BusinessCapability>>(jsonData) ?? new List<BusinessCapability>();
 
+            // Initialize Level 1 options
             Level1Options = businessCapabilities
                 .Where(bc => bc.BusinessCapabilityLevel == 1)
                 .Select(bc => new SelectListItem { Value = bc.BusinessCapabilittyName, Text = bc.BusinessCapabilittyName })
@@ -34,26 +36,26 @@ namespace x_siemins_dropdown.Pages
         public List<string> SelectedLevel1 { get; set; }
         public List<string> SelectedLevel2 { get; set; }
         public List<string> SelectedLevel3 { get; set; }
-
-        public IActionResult OnPost(List<string> selectedLevel1, List<string> selectedLevel2, List<string> selectedLevel3)
+      
+        public IActionResult OnGetShowSelected(string[] selectedLevel1, string[] selectedLevel2, string[] selectedLevel3)
         {
-            SelectedLevel1 = selectedLevel1.Select(x => x.Split('.')[0]).ToList();
+            // Process selected levels and return the result
+            var result = new
+            {
+                SelectedLevel1 = string.Join(", ", selectedLevel1.Select(x => x.Split('.')[0]).ToList()),
+                SelectedLevel2 = string.Join(", ", selectedLevel2.Select(x => x.Split('.')[0] + '.' + x.Split('.')[1]).ToList()),
+                SelectedLevel3 = string.Join(", ", selectedLevel3.Select(x => x.Split('.')[0] + '.' + x.Split('.')[1] + '.' + x.Split('.')[2]).ToList())
+            };
 
-            SelectedLevel2 = selectedLevel2.Select(x => x.Split('.')[0] + '.' + x.Split('.')[1]).ToList();
-
-            SelectedLevel3 = selectedLevel3.Select(x => x.Split('.')[0] + '.' + x.Split('.')[1] + '.' + x.Split('.')[2]).ToList();
-
-            
-            return Page();
+            return new JsonResult(result);
         }
 
         public IActionResult OnGetLoadLevel2Dropdown(string[] selectedLevel1)
         {
-
             var selected = selectedLevel1.Select(x => x.Split('.')[0]).ToList();
 
-
-            var level2Options = this.businessCapabilities
+            // Filter Level 2 options based on selected Level 1
+            var level2Options = businessCapabilities
                 .Where(bc => bc.BusinessCapabilityLevel == 2 && selected.Any(x => bc.BusinessCapabilittyName.StartsWith(x)))
                 .Select(bc => new SelectListItem { Value = bc.BusinessCapabilittyName, Text = bc.BusinessCapabilittyName })
                 .ToList();
@@ -65,14 +67,14 @@ namespace x_siemins_dropdown.Pages
         {
             var selected = selectedLevel2.Select(x => x.Split('.')[0] + '.' + x.Split('.')[1]).ToList();
 
-            var level3Options = this.businessCapabilities
+            // Filter Level 3 options based on selected Level 2
+            var level3Options = businessCapabilities
                 .Where(bc => bc.BusinessCapabilityLevel == 3 && selected.Any(x => bc.BusinessCapabilittyName.StartsWith(x)))
                 .Select(bc => new SelectListItem { Value = bc.BusinessCapabilittyName, Text = bc.BusinessCapabilittyName })
                 .ToList();
 
             return new JsonResult(level3Options);
         }
-
     }
 
     public class BusinessCapability
